4e2f7f30fc47cd359a947bf7c20daa49d8a3da1f
Anderson Salas <andersonsalasjq@gmail.com>
2025-02-21 01:51:04 -0400

    Agregando Logica de la aplicacion

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let frends = [];
+let IngresandoAmigos;
+let CantidadAmigosSecretos =5;
+
+
+function agregarAmigo() {
+
+    IngresandoAmigos = document.getElementById('amigo').value;
+    if (frends.length == CantidadAmigosSecretos){
+        document.getElementById('amigo').value = '';
+        alert("Cantidad MAxima ingresada");
+    }
+    else
+    {
+        frends.push(IngresandoAmigos);
+        console.log(frends);
+        document.getElementById('amigo').value = '';
+    }
+    
+    
+
+    
+
+    
+    
+
+    
+}
+//agregarAmigo();
