fe77d68a92e75f7970bc9f32e8c4c74c1de6b4d2
Anderson Salas <andersonsalasjq@gmail.com>
2025-02-25 00:33:47 -0400

    Agregue todas las funciones para la logica del juego

--- app.js
+++ app.js
@@ -2,7 +2,11 @@
 
 let frends = [];
 let IngresandoAmigos;
-let CantidadAmigosSecretos =5;
+let CantidadAmigosSecretos = 5;
+let indiceAmigoSecreto;
+let nombreAmigoSecreto;
+let lista;
+let creli;
 
 
 function agregarAmigo() {//Funcion para agregar amigos
@@ -15,17 +19,49 @@ function agregarAmigo() {//Funcion para agregar amigos
     else//Si aun no se cumple la condicion continua agregado
     {
         frends.push(IngresandoAmigos);
-        console.log(frends);
-        document.getElementById('amigo').value = '';
+        //console.log(frends);
+        document.getElementById('amigo').value = '';//Limpiar campo
     }
-    
-    
+        recorrerLista();
+    }
 
-    
+    function recorrerLista() {
+        lista = document.getElementById("listaAmigos");//Llama a la lista en HTml
+        lista.innerHTML = "";
 
-    
-    
+        for (let i = 0; i <= frends.length; i++) {//Recorre el array para ir agregando a la lista de HTML.
+            creli = document.createElement("li");
+            creli.textContent = frends[i];
+            lista.appendChild(creli);//Agrega el nombre al "li"
 
-    
-}
-//agregarAmigo();
+            
+            
+        }
+        
+    }
+
+    function sortearAmigo() {
+        if (frends != ""){
+            //console.log("No esta vacio");
+            indiceAmigoSecreto = Math.floor(Math.random()*frends.length);
+            nombreAmigoSecreto = frends[indiceAmigoSecreto];
+            console.log("Tu amigo secreto es: "+nombreAmigoSecreto);
+            
+            lista.innerHTML = "";
+            lista = document.getElementById("listaAmigos");
+            creli = document.createElement("li");
+            creli.textContent = nombreAmigoSecreto;
+            lista.appendChild(creli);
+            
+
+
+
+
+
+        }
+        else{
+            alert("Debe de ingresar por lo menos un amigo");
+        }
+        //lista.appendChild(nombreAmigoSecreto);
+        
+    }
